85da8e96195c86e6e90764e769d43b8597ccc6e2
Graham Gilpatric <grahamwithag@gmail.com>
2018-06-28 23:50:48 -0400

    Server and client start concurrently

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,11 +2,41 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      text: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/hello')
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      this.setState({ text: body.data });
+    })
+    .catch(error => console.error (`Error in fetch: ${error.message}`));
+  }
+
   render() {
     return (
-      <div>
-        Hello
-      </div>
+      <React.Fragment>
+        <div>
+          Hello
+        </div>
+        <div>
+          {this.state.text}
+        </div>
+      </React.Fragment>
     );
   }
 }
